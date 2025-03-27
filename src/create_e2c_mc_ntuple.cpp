728186ac2eaf74acb532889fbd8724cdc5aa8c4d
esteban <emolinac92@gmail.com>
2025-03-27 17:20:22 -0400

    fix : only mc for reference

--- src/create_e2c_mc_ntuple.cpp
+++ src/create_e2c_mc_ntuple.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "TZJetsMC.h"
 #include "TZJetsMC.C"
+#include "TZJetsMCRef.h"
+#include "TZJetsMCRef.C"
 #include "TZJetsMCReco.h"
 #include "TZJetsMCReco.C"
 #include "TZJetsData.h"
@@ -22,15 +24,8 @@ int main()
   TFile* fout = new TFile((output_folder+namef_ntuple_mc_e2c).c_str(),"RECREATE");
   
   // Declare the TTrees to be used to build the ntuples
-  TZJetsMC* mctree = new TZJetsMC();
-  TZJetsMCReco* mcrecotree = new TZJetsMCReco();
+  TZJetsMCRef* mctree = new TZJetsMCRef();
   
-  // Create Ntuples
-  TNtuple* ntuple_mcreco     = new TNtuple(name_ntuple_mcreco.c_str()    ,"Reco Sim" ,ntuple_mcreco_vars);
-  TNtuple* ntuple_mcreco_jet = new TNtuple(name_ntuple_mcreco_jet.c_str(),"Reco Sim","jet_pt");
-  ntuple_mcreco->SetAutoSave(0);
-  ntuple_mcreco_jet->SetAutoSave(0);
-
   // Create Ntuples
   TNtuple* ntuple_mc     = new TNtuple(name_ntuple_mc.c_str()    ,"MC Sim",ntuple_mc_vars);
   TNtuple* ntuple_mc_jet = new TNtuple(name_ntuple_mc_jet.c_str(),"MC Sim","jet_pt");
@@ -155,118 +150,9 @@ int main()
     last_eventNum = mctree->eventNumber;
   }
 
-  float vars[Nvars_mcreco];
-  // Fill the MCReco TNtuple
-  for(int evt = 0 ; evt < mcrecotree->fChain->GetEntries() ; evt++)
-  {
-    // Access entry of tree
-    mcrecotree->GetEntry(evt);
-
-    if(evt%10000==0)
-    {
-      double percentage = 100.*evt/mcrecotree->fChain->GetEntries();
-      std::cout<<"\r"<<percentage<<"\% jets processed."<< std::flush;
-    }
-    
-    if (evt != 0){if (last_eventNum == mcrecotree->eventNumber) continue;}
-
-    // -999 means there is not matched jet
-    if(mcrecotree->Jet_mcjet_nmcdtrs==-999) continue;
-
-    // Apply PV cut
-    if(mcrecotree->nPV!=1) continue;
-
-    // Apply trigger cut
-    bool mum_trigger = (mcrecotree->mum_L0MuonEWDecision_TOS==1&&mcrecotree->mum_Hlt1SingleMuonHighPTDecision_TOS==1&&mcrecotree->mum_Hlt2EWSingleMuonVHighPtDecision_TOS==1);
-    bool mup_trigger = (mcrecotree->mup_L0MuonEWDecision_TOS==1&&mcrecotree->mup_Hlt1SingleMuonHighPTDecision_TOS==1&&mcrecotree->mup_Hlt2EWSingleMuonVHighPtDecision_TOS==1);
-
-    if(!mum_trigger&&!mup_trigger) continue;
-
-    // Set Jet-associated 4 vectors and apply cuts
-    Jet_4vector->SetPxPyPzE(mcrecotree->Jet_PX/1000.,mcrecotree->Jet_PY/1000.,mcrecotree->Jet_PZ/1000.,mcrecotree->Jet_PE/1000.);
-    if(!apply_jet_cuts(Jet_4vector->Eta(),Jet_4vector->Pt())) continue;
-    
-    mum_4vector->SetPxPyPzE(mcrecotree->mum_PX/1000.,mcrecotree->mum_PY/1000.,mcrecotree->mum_PZ/1000.,mcrecotree->mum_PE/1000.);
-    if(!apply_muon_cuts(Jet_4vector->DeltaR(*mum_4vector),mum_4vector->Pt(),mum_4vector->Eta())) continue;
-    
-    mup_4vector->SetPxPyPzE(mcrecotree->mup_PX/1000.,mcrecotree->mup_PY/1000.,mcrecotree->mup_PZ/1000.,mcrecotree->mup_PE/1000.);
-    if(!apply_muon_cuts(Jet_4vector->DeltaR(*mup_4vector),mup_4vector->Pt(),mup_4vector->Eta())) continue;
-    
-    Z0_4vector->SetPxPyPzE(mup_4vector->Px()+mum_4vector->Px(),mup_4vector->Py()+mum_4vector->Py(),mup_4vector->Pz()+mum_4vector->Pz(),mup_4vector->E() +mum_4vector->E());
-    if(!apply_zboson_cuts(TMath::Abs(Jet_4vector->DeltaPhi(*Z0_4vector)),Z0_4vector->M())) continue;
-
-    ntuple_mcreco_jet->Fill(Jet_4vector->Pt());
-            
-    // Loop over hadron 1
-    for(int h1_index = 0 ; h1_index < mcrecotree->Jet_NDtr ; h1_index++)
-    {
-        // Skip non-hadronic particles
-        if(mcrecotree->Jet_Dtr_IsMeson[h1_index]!=1&&mcrecotree->Jet_Dtr_IsBaryon[h1_index]!=1) continue;
-
-        h1_4vector->SetPxPyPzE(mcrecotree->Jet_Dtr_PX[h1_index]/1000.,mcrecotree->Jet_Dtr_PY[h1_index]/1000.,mcrecotree->Jet_Dtr_PZ[h1_index]/1000.,mcrecotree->Jet_Dtr_E[h1_index]/1000.);
-        if(!apply_chargedtrack_cuts(mcrecotree->Jet_Dtr_ThreeCharge[h1_index],
-                                    mcrecotree->Jet_Dtr_P[h1_index]/1000.,
-                                    mcrecotree->Jet_Dtr_PT[h1_index]/1000.,
-                                    mcrecotree->Jet_Dtr_TrackChi2[h1_index]/mcrecotree->Jet_Dtr_TrackNDF[h1_index],
-                                    mcrecotree->Jet_Dtr_ProbNNghost[h1_index],
-                                    Jet_4vector->DeltaR(*h1_4vector))) continue;
-
-        // Loop over hadron 2
-        for(int h2_index = h1_index+1 ; h2_index < mcrecotree->Jet_NDtr ; h2_index++)
-        {
-            // Skip non-hadronic particles
-            if(mcrecotree->Jet_Dtr_IsMeson[h2_index]!=1&&mcrecotree->Jet_Dtr_IsBaryon[h2_index]!=1) continue;
-
-            h2_4vector->SetPxPyPzE(mcrecotree->Jet_Dtr_PX[h2_index]/1000.,mcrecotree->Jet_Dtr_PY[h2_index]/1000.,mcrecotree->Jet_Dtr_PZ[h2_index]/1000.,mcrecotree->Jet_Dtr_E[h2_index]/1000.);
-            if(!apply_chargedtrack_cuts(mcrecotree->Jet_Dtr_ThreeCharge[h2_index],
-                                        mcrecotree->Jet_Dtr_P[h2_index]/1000.,
-                                        mcrecotree->Jet_Dtr_PT[h2_index]/1000.,
-                                        mcrecotree->Jet_Dtr_TrackChi2[h2_index]/mcrecotree->Jet_Dtr_TrackNDF[h2_index],
-                                        mcrecotree->Jet_Dtr_ProbNNghost[h2_index],
-                                        Jet_4vector->DeltaR(*h2_4vector))) continue;
-
-            double h1_y = rapidity(mcrecotree->Jet_Dtr_E[h1_index],mcrecotree->Jet_Dtr_PZ[h1_index]); 
-            double h2_y = rapidity(mcrecotree->Jet_Dtr_E[h2_index],mcrecotree->Jet_Dtr_PZ[h2_index]);
-
-            // If all good, fille Ntuple
-            vars[0]  = weight(mcrecotree->Jet_Dtr_E[h1_index], mcrecotree->Jet_Dtr_E[h2_index], mcrecotree->Jet_PE);
-            vars[1]  = R_L(h1_y, h2_y, mcrecotree->Jet_Dtr_PHI[h1_index], mcrecotree->Jet_Dtr_PHI[h2_index]);
-            vars[2]  = mcrecotree->Jet_Dtr_ETA[h1_index];
-            vars[3]  = mcrecotree->Jet_Dtr_ETA[h2_index];
-            vars[4]  = h1_y;
-            vars[5]  = h2_y;
-            vars[6]  = mcrecotree->Jet_Dtr_ThreeCharge[h1_index];
-            vars[7]  = mcrecotree->Jet_Dtr_ThreeCharge[h2_index];
-            vars[8]  = mcrecotree->Jet_Dtr_P[h1_index]/1000.;
-            vars[9]  = mcrecotree->Jet_Dtr_P[h2_index]/1000.;
-            vars[10] = mcrecotree->Jet_Dtr_PT[h1_index]/1000.;
-            vars[11] = mcrecotree->Jet_Dtr_PT[h2_index]/1000.;
-            vars[12] = mcrecotree->Jet_PT/1000.;
-            vars[13] = Jet_4vector->Eta();
-            vars[14] = weight(mcrecotree->Jet_Dtr_PT[h1_index], mcrecotree->Jet_Dtr_PT[h2_index], mcrecotree->Jet_PT);
-            vars[15] = delta_phi(Jet_4vector->Phi(),Z0_4vector->Phi());
-            vars[16] = R_L(Jet_4vector->Rapidity(),mum_4vector->Rapidity(),Jet_4vector->Phi(),mum_4vector->Phi());
-            vars[17] = mum_4vector->Pt();
-            vars[18] = mum_4vector->Eta();
-            vars[19] = R_L(Jet_4vector->Rapidity(),mup_4vector->Rapidity(),Jet_4vector->Phi(),mup_4vector->Phi());
-            vars[20] = mup_4vector->Pt();
-            vars[21] = mup_4vector->Eta();
-            vars[22] = mcrecotree->Jet_Dtr_ID[h1_index];
-            vars[23] = mcrecotree->Jet_Dtr_ID[h2_index];
-            
-            // Fill the TNtuple
-            ntuple_mcreco->Fill(vars);
-        }
-    }
-
-    last_eventNum = mcrecotree->eventNumber;
-  }
-
   fout->cd();
   ntuple_mc->Write();
   ntuple_mc_jet->Write();
-  ntuple_mcreco->Write();
-  ntuple_mcreco_jet->Write();
   fout->Close();
 
   std::cout<<std::endl;
--- src/macro_makeclasses.cpp
+++ src/macro_makeclasses.cpp
@@ -10,6 +10,7 @@ void macro_makeclasses()
     TChain* chain2 = new TChain("StdHltZJets/DecayTree");
     TChain* chain3 = new TChain("StdHltZJets/DecayTree");    // Used for data
     TChain* chain4 = new TChain("mcjettuple/MCJetTree");  
+    TChain* chain5 = new TChain("mcjettuple/MCJetTree");  
 
     chain1->Add((input_folder+"Zjet_MC_Sim09j_2016_MD_03112025_full.root").c_str());
     chain1->Add((input_folder+"Zjet_MC_Sim09j_2016_MU_03112025_full.root").c_str());
@@ -26,9 +27,13 @@ void macro_makeclasses()
     chain4->Add((input_folder+"Zjet_MC_Sim09j_2016_MD_03252025_0_full.root").c_str());
     chain4->Add((input_folder+"Zjet_MC_Sim09j_2016_MU_03252025_0_full.root").c_str());
     
+    chain5->Add((input_folder+"Zjet_MC_Sim09l_2016_MD_03112025_full.root").c_str());
+    chain5->Add((input_folder+"Zjet_MC_Sim09l_2016_MU_03112025_full.root").c_str());
+    
     chain1->MakeClass("TZJetsMC");
     chain2->MakeClass("TZJetsMCReco");
     chain3->MakeClass("TZJetsData");
     chain4->MakeClass("TZJetsMCAT");
+    chain5->MakeClass("TZJetsMCRef");
 }
 
