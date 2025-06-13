2c61d2e75993cc6e949317a1582f36585d095043
esteban <emolinac92@gmail.com>
2025-06-13 12:24:27 -0400

    refactor : changed directories and files to extract data

--- src/macro_makeclasses.cpp
+++ src/macro_makeclasses.cpp
@@ -39,12 +39,12 @@ void macro_makeclasses()
     chain2->Add((input_folder+"Zjet_MC_Sim10a_2018_MD_04142025.root").c_str());
     chain2->Add((input_folder+"Zjet_MC_Sim10a_2018_MU_04142025.root").c_str());
     
-    chain3->Add((input_folder+"Zjet_Data_2016_MU_04062024.root").c_str());
-    chain3->Add((input_folder+"Zjet_Data_2016_MD_04062024.root").c_str());
-    chain4->Add((input_folder+"Zjet_Data_2017_MU_04062024.root").c_str());
-    chain4->Add((input_folder+"Zjet_Data_2017_MD_04062024.root").c_str());
-    chain5->Add((input_folder+"Zjet_Data_2018_MU_04062024.root").c_str());
-    chain5->Add((input_folder+"Zjet_Data_2018_MD_04062024.root").c_str());
+    chain3->Add((input_folder+"Zjet_Data_2016_MU_04212025.root").c_str());
+    chain3->Add((input_folder+"Zjet_Data_2016_MD_04212025.root").c_str());
+    chain4->Add((input_folder+"Zjet_Data_2017_MU_04222025.root").c_str());
+    chain4->Add((input_folder+"Zjet_Data_2017_MD_04222025.root").c_str());
+    chain5->Add((input_folder+"Zjet_Data_2018_MU_04232025.root").c_str());
+    chain5->Add((input_folder+"Zjet_Data_2018_MD_04242025.root").c_str());
 
     chain1->MakeClass("TZJetsMC");
     chain2->MakeClass("TZJetsMCReco");
