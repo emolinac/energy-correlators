a3b9710ae93eca02ad58fee0bfe2c17b2e22b4e0
esteban <emolinac92@gmail.com>
2025-07-21 12:09:23 -0400

    refactor! : cleaned functions no longer in use

--- include/analysis-functions.cpp
+++ include/analysis-functions.cpp
@@ -3,10 +3,4 @@
 
 double weight(double h1_E, double h2_E, double jet_E);
 
-double R_L(double h1_y, double h2_y, double h1_phi, double h2_phi);
-
-double delta_phi(double h1_phi, double h2_phi);
-
-double rapidity(double energy, double pz);
-
 #endif
