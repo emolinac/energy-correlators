219443e940547e5ca0eff7a19e1eefe17484300c
esteban <emolinac92@gmail.com>
2025-07-21 14:06:09 -0400

    style: linux kernel coding style

--- include/utils-algorithms.h
+++ include/utils-algorithms.h
@@ -30,7 +30,7 @@ double get_hwhm(TH1F* h)
     
         int halfwidth_bin;
         for (int bin = 1 ; bin <= h->GetNbinsX() ; bin++)
-                if (h->GetBinContent(bin)>=half_max) {
+                if (h->GetBinContent(bin) >= half_max) {
                         halfwidth_bin = bin; 
                         break;
                 }
@@ -50,7 +50,7 @@ void set_histo_with_systematics(TH1F* hdeviations, TH1F* hnominal, TH1F* hsystem
                 double dev_err = hdeviations->GetBinError(hbin);
 
                 // Demand more than one sigma to be considered
-                if ((dev+dev_err>1&&dev<1)||(dev-dev_err<1&&dev>1)) 
+                if ((dev+dev_err > 1 && dev < 1) || (dev-dev_err < 1 && dev > 1)) 
                         continue;
 
                 double syst_error;
@@ -70,33 +70,32 @@ void set_histo_with_systematics(TH1F* hdeviations, TH1F* hnominal, TH1F* hsystem
                 total     += hnominal->GetBinContent(hbin);
         }
 
-        if (total > 0) {
+        if (total > 0)
                 std::cout<<total_err*100./total;
-        }
 }
 
 void set_histo_sqrt_content(TH1F* h)
 {
         for (int i = 1 ; i <= h->GetNbinsX() ; i++) {
-                h->SetBinContent(i,sqrt(h->GetBinContent(i)));
-                h->SetBinError(i,sqrt(h->GetBinError(i)));
+                h->SetBinContent(i, sqrt(h->GetBinContent(i)));
+                h->SetBinError(i, sqrt(h->GetBinError(i)));
         }
 }
 
 void set_histo_null_errors(TH1F* h)
 {
         for (int i = 1 ; i <= h->GetNbinsX() ; i++)
-                h->SetBinError(i,0);
+                h->SetBinError(i, 0);
 }
 
 void set_shift_histo(TH2F* href, TH2F* hshift, TRandom3* rndm)
 {
         for (int xbin = 1 ; xbin <= href->GetNbinsX() ; xbin++) {
                 for (int ybin = 1 ; ybin <= href->GetNbinsY() ; ybin++) {
-                        double shift_window = href->GetBinError(href->GetBin(xbin,ybin));
-                        double refdata      = href->GetBinContent(href->GetBin(xbin,ybin));
-                        double shift        = rndm->Gaus(1,abs(shift_window/refdata));
-                        hshift->SetBinContent(xbin,ybin,shift);
+                        double shift_window = href->GetBinError(href->GetBin(xbin, ybin));
+                        double refdata      = href->GetBinContent(href->GetBin(xbin, ybin));
+                        double shift        = rndm->Gaus(1, abs(shift_window/refdata));
+                        hshift->SetBinContent(xbin, ybin, shift);
                 }
         }
 }
@@ -105,33 +104,33 @@ void set_shift_histo(TH2D* href, TH2D* hshift, TRandom3* rndm)
 {
         for (int xbin = 1 ; xbin <= href->GetNbinsX() ; xbin++) {
                 for (int ybin = 1 ; ybin <= href->GetNbinsY() ; ybin++) {
-                        double shift_window = href->GetBinError(href->GetBin(xbin,ybin));
-                        double refdata      = href->GetBinContent(href->GetBin(xbin,ybin));
-                        double shift        = rndm->Gaus(refdata,shift_window)/refdata; //rndm->Gaus(1,abs(shift_window/refdata));
-                        hshift->SetBinContent(xbin,ybin,shift);
+                        double shift_window = href->GetBinError(href->GetBin(xbin, ybin));
+                        double refdata      = href->GetBinContent(href->GetBin(xbin, ybin));
+                        double shift        = rndm->Gaus(refdata, shift_window)/refdata; //rndm->Gaus(1,abs(shift_window/refdata));
+                        hshift->SetBinContent(xbin, ybin, shift);
                 }
         }
 }
 
 void set_data_ntuple_branches(TNtuple* ntuple, float* R_L, float* jet_pt, float* weight_pt, float* efficiency, float* purity, float* efficiency_relerror, float* purity_relerror)
 {
-        ntuple->SetBranchAddress("R_L",R_L);
-        ntuple->SetBranchAddress("jet_pt",jet_pt);
-        ntuple->SetBranchAddress("weight_pt",weight_pt);
-        ntuple->SetBranchAddress("efficiency",efficiency);
-        ntuple->SetBranchAddress("efficiency_relerror",efficiency_relerror);
-        ntuple->SetBranchAddress("purity",purity);
-        ntuple->SetBranchAddress("purity_relerror",purity_relerror);
+        ntuple->SetBranchAddress("R_L", R_L);
+        ntuple->SetBranchAddress("jet_pt", jet_pt);
+        ntuple->SetBranchAddress("weight_pt", weight_pt);
+        ntuple->SetBranchAddress("efficiency", efficiency);
+        ntuple->SetBranchAddress("efficiency_relerror", efficiency_relerror);
+        ntuple->SetBranchAddress("purity", purity);
+        ntuple->SetBranchAddress("purity_relerror", purity_relerror);
 }
 
 void set_unfolding_ntuple_branches(TNtuple* ntuple, float* R_L_reco, float* R_L_truth, float* jet_pt_reco, float* jet_pt_truth, float* weight_pt_reco, float* weight_pt_truth)
 {
-        ntuple->SetBranchAddress("jet_pt",jet_pt_reco);
-        ntuple->SetBranchAddress("jet_pt_truth",jet_pt_truth);
-        ntuple->SetBranchAddress("R_L",R_L_reco);
-        ntuple->SetBranchAddress("R_L_truth",R_L_truth);
-        ntuple->SetBranchAddress("weight_pt",weight_pt_reco);
-        ntuple->SetBranchAddress("weight_pt_truth",weight_pt_truth);
+        ntuple->SetBranchAddress("jet_pt", jet_pt_reco);
+        ntuple->SetBranchAddress("jet_pt_truth", jet_pt_truth);
+        ntuple->SetBranchAddress("R_L", R_L_reco);
+        ntuple->SetBranchAddress("R_L_truth", R_L_truth);
+        ntuple->SetBranchAddress("weight_pt", weight_pt_reco);
+        ntuple->SetBranchAddress("weight_pt_truth", weight_pt_truth);
 }
 
 #endif
--- include/utils-visual.h
+++ include/utils-visual.h
@@ -3,28 +3,28 @@
 
 void set_histogram_style(TH1* h, int color, int line_width, int marker, double marker_size)
 {
-    h->SetLineColor(color);
-    h->SetLineWidth(line_width);
-    h->SetMarkerColor(color);
-    h->SetMarkerStyle(marker);
-    h->SetMarkerSize(marker_size);
+        h->SetLineColor(color);
+        h->SetLineWidth(line_width);
+        h->SetMarkerColor(color);
+        h->SetMarkerStyle(marker);
+        h->SetMarkerSize(marker_size);
 }
 
 void draw_lhcb_tag(TLatex* latex)
 {
-    latex->SetLineWidth(2);
-    latex->DrawLatexNDC(0.75,0.85,"#font[22]{LHCb}");
-    latex->DrawLatexNDC(0.75,0.80,"#font[22]{p-p collisions}");
-    latex->DrawLatexNDC(0.75,0.75,"#font[22]{#sqrt{s} = 13 TeV}");
-    latex->DrawLatexNDC(0.75,0.70,"#font[22]{Z-Tagged Jets}");
+        latex->SetLineWidth(2);
+        latex->DrawLatexNDC(0.75, 0.85, "#font[22]{LHCb}");
+        latex->DrawLatexNDC(0.75, 0.80, "#font[22]{p-p collisions}");
+        latex->DrawLatexNDC(0.75, 0.75, "#font[22]{#sqrt{s} = 13 TeV}");
+        latex->DrawLatexNDC(0.75, 0.70, "#font[22]{Z-Tagged Jets}");
 }
 
 void set_lhcb_watermark_properties(TLatex* latex)
 {
-    latex->SetTextColorAlpha(16,0.3);
-    latex->SetTextSize(0.1991525);
-    latex->SetTextAngle(26.15998);
-    latex->SetLineWidth(2);
+        latex->SetTextColorAlpha(16, 0.3);
+        latex->SetTextSize(0.1991525);
+        latex->SetTextAngle(26.15998);
+        latex->SetLineWidth(2);
 }
 
 #endif
