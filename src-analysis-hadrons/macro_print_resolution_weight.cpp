2a54741b5c165e1374c74fda1917b0b8b72fd341
esteban <emolinac92@gmail.com>
2025-07-22 12:38:08 -0400

    style!: implemented linux kernel coding style on src-analysis-hadrons refactor: moved the codes associated to single track correction visuals to a special folder

--- src-analysis-hadrons/macro_print_resolution_weight.cpp
+++ src-analysis-hadrons/macro_print_resolution_weight.cpp
@@ -8,86 +8,100 @@
 
 void macro_print_resolution_weight()
 {
-    gStyle->SetOptStat(1110);
+        gStyle->SetOptStat(1110);
 
-    // Open the necessary files
-    TFile* fpurity = new TFile((output_folder + namef_ntuple_e2c_pairpurity).c_str());
+        // Open the necessary files
+        TFile* fpurity = new TFile((output_folder + namef_ntuple_e2c_pairpurity).c_str());
 
-    // Get the corresponding Ntuples
-    TNtuple* ntuple_dtrmatch = (TNtuple*) fpurity->Get((name_ntuple_purity).c_str());
+        // Get the corresponding Ntuples
+        TNtuple* ntuple_dtrmatch = (TNtuple*) fpurity->Get((name_ntuple_purity).c_str());
 
-    // Define the necessary histograms to calculate purity
-    // TH1F* hres = new TH1F("hres","",200,-.06,.06);
-    TH1F* hres   = new TH1F("hres","",1000,-.4,.4);
-    TH1F* hratio = new TH1F("hratio","",1000,0.,3);
-    hres->Sumw2();
-    hratio->Sumw2();
-    set_histogram_style(hres, kViolet, std_line_width, std_marker_style, std_marker_size);
-    set_histogram_style(hratio, kViolet, std_line_width, std_marker_style, std_marker_size);
-    
-    // Project into the histograms
-    ntuple_dtrmatch->Project("hres"  ,"weight_pt-weight_pt_truth",pair_cut+"weight_pt!=0&&weight_pt_truth!=-999");
-    ntuple_dtrmatch->Project("hratio","weight_pt/weight_pt_truth",pair_cut+"weight_pt!=0&&weight_pt_truth!=-999");
-    
-    // Calculate event fractions 
-    double total = hres->Integral();
-    
-    std::cout<<"There are "<<total<<" entries."<<std::endl;
-    std::cout<<"For resolution:"<<std::endl;
-    for (int bin = 1 ; bin <= hres->GetNbinsX()/2. ; bin++)
-    {
-        double integral = hres->Integral(bin,hres->GetNbinsX()-bin);
-        if (integral/total < 0.95) {std::cout<<"Between "<<hres->GetBinCenter(bin)<<" and "<<hres->GetBinCenter(hres->GetNbinsX()-bin)<<" is the 95%% of the sample"<<std::endl; break;}
-    }
-    for (int bin = 1 ; bin <= hres->GetNbinsX()/2 ; bin++)
-    {
-        double integral = hres->Integral(bin,hres->GetNbinsX()-bin);
-        if (integral/total < 0.68) {std::cout<<"Between "<<hres->GetBinCenter(bin)<<" and "<<hres->GetBinCenter(hres->GetNbinsX()-bin)<<" is the 68%% of the sample"<<std::endl; break;}
-    }
-    std::cout<<"For resolutionas a percentage:"<<std::endl;
-    for (int bin = 1 ; bin <= hratio->GetNbinsX()/2. ; bin++)
-    {
-        double integral = hratio->Integral(bin,hratio->GetNbinsX()-bin);
-        if (integral/total < 0.95) {std::cout<<"Between "<<hratio->GetBinCenter(bin)<<" and "<<hratio->GetBinCenter(hratio->GetNbinsX()-bin)<<" is the 95%% of the sample"<<std::endl; break;}
-    }
-    
-    // Draw
-    TCanvas* c = new TCanvas("c","",800,600);
-    c->Draw();
+        // Define the necessary histograms to calculate purity
+        // TH1F* hres = new TH1F("hres","",200,-.06,.06);
+        TH1F* hres   = new TH1F("hres","",1000,-.4,.4);
+        TH1F* hratio = new TH1F("hratio","",1000,0.,3);
+        hres->Sumw2();
+        hratio->Sumw2();
+        set_histogram_style(hres, kViolet, std_line_width, std_marker_style, std_marker_size);
+        set_histogram_style(hratio, kViolet, std_line_width, std_marker_style, std_marker_size);
+        
+        // Project into the histograms
+        ntuple_dtrmatch->Project("hres"  ,"weight_pt-weight_pt_truth",pair_cut+"weight_pt!=0&&weight_pt_truth!=-999");
+        ntuple_dtrmatch->Project("hratio","weight_pt/weight_pt_truth",pair_cut+"weight_pt!=0&&weight_pt_truth!=-999");
+        
+        // Calculate event fractions 
+        double total = hres->Integral();
+        
+        std::cout<<"There are "<<total<<" entries."<<std::endl;
+        std::cout<<"For resolution:"<<std::endl;
+        for (int bin = 1 ; bin <= hres->GetNbinsX()/2. ; bin++) {
+                double integral = hres->Integral(bin,hres->GetNbinsX()-bin);
 
-    TLatex* tex = new TLatex();
-    // tex->SetTextColorAlpha(16,0.3);
-    // tex->SetTextSize(0.1991525);
-    // tex->SetTextAngle(26.15998);
-    tex->SetLineWidth(2);
+                if (integral/total < 0.95) {
+                        std::cout<<"Between "<<hres->GetBinCenter(bin)<<" and "<<hres->GetBinCenter(hres->GetNbinsX()-bin)<<" is the 95%% of the sample"<<std::endl; 
+                        
+                        break;
+                }
+        }
 
-    hres->Draw();
-    hres->SetTitle(";#Delta weight(Reco,Truth);");
+        for (int bin = 1 ; bin <= hres->GetNbinsX()/2 ; bin++) {
+                double integral = hres->Integral(bin,hres->GetNbinsX()-bin);
+                
+                if (integral/total < 0.68) {
+                        std::cout<<"Between "<<hres->GetBinCenter(bin)<<" and "<<hres->GetBinCenter(hres->GetNbinsX()-bin)<<" is the 68%% of the sample"<<std::endl; 
+                        
+                        break;
+                }
+        }
 
-    gPad->SetLogy(1);
+        std::cout<<"For resolutionas a percentage:"<<std::endl;
+        for (int bin = 1 ; bin <= hratio->GetNbinsX()/2. ; bin++) {
+                double integral = hratio->Integral(bin,hratio->GetNbinsX()-bin);
 
-    TLine* line1 = new TLine();
-    TLine* line2 = new TLine();
-    line1->SetLineColorAlpha(3,0.4);
-    line1->SetLineStyle(9);
-    line2->SetLineColorAlpha(3,0.4);
-    line2->SetLineStyle(9);
+                if (integral/total < 0.95) {
+                        std::cout<<"Between "<<hratio->GetBinCenter(bin)<<" and "<<hratio->GetBinCenter(hratio->GetNbinsX()-bin)<<" is the 95%% of the sample"<<std::endl; 
+                        
+                        break;
+                }
+        }
+        
+        // Draw
+        TCanvas* c = new TCanvas("c","",800,600);
+        c->Draw();
 
-    // line1->DrawLine(hres->GetBinCenter(hres->GetMaximumBin())-get_hwhm(hres),0,hres->GetBinCenter(hres->GetMaximumBin())-get_hwhm(hres),hres->GetMaximum());
-    // line2->DrawLine(hres->GetBinCenter(hres->GetMaximumBin())+get_hwhm(hres),0,hres->GetBinCenter(hres->GetMaximumBin())+get_hwhm(hres),hres->GetMaximum());
-    
-    // tex->DrawLatexNDC(0.2,0.75,Form("HWHM = %.5f",get_hwhm(hres)));
+        TLatex* tex = new TLatex();
+        // tex->SetTextColorAlpha(16,0.3);
+        // tex->SetTextSize(0.1991525);
+        // tex->SetTextAngle(26.15998);
+        tex->SetLineWidth(2);
 
-    c->Print("./plots/resolution_weight.pdf");
+        hres->Draw();
+        hres->SetTitle(";#Delta weight(Reco,Truth);");
 
-    hratio->Draw();
-    hratio->SetTitle(";weight(Reco)/weight(Truth);");
+        gPad->SetLogy(1);
 
-    // line1->DrawLine(hratio->GetBinCenter(hratio->GetMaximumBin())-get_hwhm(hratio),0,hratio->GetBinCenter(hratio->GetMaximumBin())-get_hwhm(hratio),hratio->GetMaximum());
-    // line2->DrawLine(hratio->GetBinCenter(hratio->GetMaximumBin())+get_hwhm(hratio),0,hratio->GetBinCenter(hratio->GetMaximumBin())+get_hwhm(hratio),hratio->GetMaximum());
+        TLine* line1 = new TLine();
+        TLine* line2 = new TLine();
+        line1->SetLineColorAlpha(3,0.4);
+        line1->SetLineStyle(9);
+        line2->SetLineColorAlpha(3,0.4);
+        line2->SetLineStyle(9);
 
-    // tex->DrawLatexNDC(0.2,0.75,Form("HWHM = %.5f",get_hwhm(hratio)));
+        // line1->DrawLine(hres->GetBinCenter(hres->GetMaximumBin())-get_hwhm(hres),0,hres->GetBinCenter(hres->GetMaximumBin())-get_hwhm(hres),hres->GetMaximum());
+        // line2->DrawLine(hres->GetBinCenter(hres->GetMaximumBin())+get_hwhm(hres),0,hres->GetBinCenter(hres->GetMaximumBin())+get_hwhm(hres),hres->GetMaximum());
+        
+        // tex->DrawLatexNDC(0.2,0.75,Form("HWHM = %.5f",get_hwhm(hres)));
 
-    // gPad->SetLogy(0);
-    c->Print("./plots/resolution_weight_ratio.pdf");
+        c->Print("./plots/resolution_weight.pdf");
+
+        hratio->Draw();
+        hratio->SetTitle(";weight(Reco)/weight(Truth);");
+
+        // line1->DrawLine(hratio->GetBinCenter(hratio->GetMaximumBin())-get_hwhm(hratio),0,hratio->GetBinCenter(hratio->GetMaximumBin())-get_hwhm(hratio),hratio->GetMaximum());
+        // line2->DrawLine(hratio->GetBinCenter(hratio->GetMaximumBin())+get_hwhm(hratio),0,hratio->GetBinCenter(hratio->GetMaximumBin())+get_hwhm(hratio),hratio->GetMaximum());
+
+        // tex->DrawLatexNDC(0.2,0.75,Form("HWHM = %.5f",get_hwhm(hratio)));
+
+        // gPad->SetLogy(0);
+        c->Print("./plots/resolution_weight_ratio.pdf");
 }
