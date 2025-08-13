e6915b8923220b414e75c669e9dbd7efe753a4d6
esteban <emolinac92@gmail.com>
2025-08-13 15:47:11 -0400

    refactor(cleaning)!

--- legacy/systematics/legacy-systematics/syst-corr-binning/run_initial_step.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Go to src, execute code, and move the respective files
-cd ./src
-root -q macro_makeclasses.cpp
-mv *.C *.h ../include
-
-echo "NOTE : Remember to set the mother and input folders in the directories.h file in the include folder!!"
